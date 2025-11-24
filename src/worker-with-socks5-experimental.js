a0bc45f474e3391509fe6de91a72d0e22fa21bda
Townwang <android@townwang.com>
2025-11-25 00:33:24 +0800

    update

--- src/worker-with-socks5-experimental.js
+++ src/worker-with-socks5-experimental.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '2ae6b565-d3f1-45bc-8752-f73e8e1e35b6';
 
 let proxyIP = '';
 
